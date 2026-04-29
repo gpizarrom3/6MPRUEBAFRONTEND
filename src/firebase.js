04de7e48fc9c324a8fbf531699f77377ddd3c483
gpizarrom3 <gpizarrom.3@gmail.com>
2026-04-29 16:57:21 -0400

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,21 +2,19 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// Tus credenciales reales extraídas de tu captura
 const firebaseConfig = {
-  apiKey: "AIzaSyAUh9aLqIcjsODagU_0XSGnczWJgK5iGUU",
-  authDomain: "gen-lang-client-0190458729.firebaseapp.com",
-  projectId: "gen-lang-client-0190458729",
-  storageBucket: "gen-lang-client-0190458729.firebasestorage.app",
-  messagingSenderId: "198510499865",
-  appId: "1:198510499865:web:bdb03240bc67a4de957288",
-  measurementId: "G-NX17L5RMMS"
+  apiKey: "AIzaSyDQwszrwEyZkGgh3Ym2yEKYgbgk0kGTn5E",
+  authDomain: "auditoria-6m-pro.firebaseapp.com",
+  projectId: "auditoria-6m-pro",
+  storageBucket: "auditoria-6m-pro.firebasestorage.app",
+  messagingSenderId: "739778301047",
+  appId: "1:739778301047:web:8eef0abab974a1be8d4a72",
+  measurementId: "G-7D0FPPE849"
 };
 
-// Inicializamos Firebase
+// Inicialización de los servicios de auditoría
 const app = initializeApp(firebaseConfig);
 
-// Exportamos los servicios que el Dashboard (App.js) necesita
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const googleProvider = new GoogleAuthProvider();
