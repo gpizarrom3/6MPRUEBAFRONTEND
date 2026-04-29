fb06a8149e66adfbbca4247b5d75d7ba9288b569
gpizarrom3 <gpizarrom.3@gmail.com>
2026-04-29 16:53:15 -0400

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,16 +2,23 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
+// Tus credenciales reales extraídas de tu captura
 const firebaseConfig = {
-  apiKey: "TU_API_KEY",
-  authDomain: "TU_DOMINIO.firebaseapp.com",
-  projectId: "TU_ID",
-  storageBucket: "TU_STORAGE.appspot.com",
-  messagingSenderId: "TU_SENDER",
-  appId: "TU_APP_ID"
+  apiKey: "AIzaSyAUh9aLqIcjsODagU_0XSGnczWJgK5iGUU",
+  authDomain: "gen-lang-client-0190458729.firebaseapp.com",
+  projectId: "gen-lang-client-0190458729",
+  storageBucket: "gen-lang-client-0190458729.firebasestorage.app",
+  messagingSenderId: "198510499865",
+  appId: "1:198510499865:web:bdb03240bc67a4de957288",
+  measurementId: "G-NX17L5RMMS"
 };
 
+// Inicializamos Firebase
 const app = initializeApp(firebaseConfig);
+
+// Exportamos los servicios que el Dashboard (App.js) necesita
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const googleProvider = new GoogleAuthProvider();
+
+export default app;
